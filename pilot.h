1c1108778262634548e100a0480e3f1317a8fdaa
Jason <39454024+JDHG@users.noreply.github.com>
2018-09-24 17:48:21 +0930

    Updted person and pilot .h and .cpp files so they can be initialised.

--- pilot.h
+++ pilot.h
@@ -2,19 +2,20 @@
 #define PILOT_H
 #include "person.h"
 
-class pilot : public person {
+class Pilot : public Person {
+	static int nPilots; //tracks number of pilots created for unique ID assignment
 	int IDNumber;
 	bool assignedPlane;
 
 public:
-	pilot();
-	pilot(int IDNumber);
+	Pilot();
+	Pilot(std::string newName, int newWeight, std::string newDestination);
 
 	//Setters
-	void set_IDNumber(int newIDNumber);
-	void set_assignedPlane();
+	void set_id_number(int newIDNumber);
+	void set_assigned_plane();
 	//Getters
-	int get_IDNumber();
-	bool get_assignedPlane();
+	int get_id_number();
+	bool get_assigned_plane();
 };
 #endif
