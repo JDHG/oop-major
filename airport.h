3f6a653303353388b00ee6f8f6707803b561f0d7
Toobs2007 <43287572+Toobs2007@users.noreply.github.com>
2018-10-04 16:48:55 +0930

    Updated airport.h
    Departure function tested and working.
    Removed planes-grounded variable.
    Using vector size instead.

--- airport.h
+++ airport.h
@@ -4,13 +4,11 @@
 #include <vector>
 #include <string>
 #include "plane.h"
-//change
 
 class Airport
 {
 
 	int maxPlanes;
-	int planesGrounded;
 	std::string location;
 	std::vector<Plane*> planesOnSite;
 	
@@ -21,13 +19,12 @@ public:
 
 	//getters
 	int get_max_planes();
-	int get_planes_grounded();
 	std::string get_location();
 	std::vector<Plane*> get_list_planes();
 
 
 
-	bool departure(Plane* depPlane, Airport * destAirport); //removes departing plane from planesOnSite array.
+	bool departure(Plane* depPlane, Airport* destAirport, int input); //removes departing plane from planesOnSite array.
 	void add_plane(Plane* newPlane);
 
 	~Airport();
